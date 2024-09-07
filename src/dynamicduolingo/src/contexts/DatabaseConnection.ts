ce21b53882775ecbcd2d8ca697085bf5d9f27086
kaua-pt <kauaponte@hotmail.com>
2024-09-06 22:22:47 -0300

    database

--- src/dynamicduolingo/src/contexts/DatabaseConnection.ts
+++ src/dynamicduolingo/src/contexts/DatabaseConnection.ts
@@ -1,9 +1,20 @@
-import * as fs from 'fs';
 
 export default class DatabaseConnection implements IDatabaseConnection {
     public GetWord(regex: string): string[] {
-        const allPtBrWords = fs.readFileSync("src//dynamicduolingo//data//br-utf8.txt", 'utf8');
-        const foundWords = allPtBrWords.match(regex);
-        return foundWords || [];
+        fetch('/br-utf8.txt')
+            .then((response) => {
+                if (!response.ok)
+                    throw new Error('Erro ao carregar o arquivo');
+                return response.text();
+            })
+            .then((text) => {
+                const lines = text.split(/\r?\n/);
+                return lines.filter((line) => regex.match(line));
+            })
+            .catch((error) => {
+                console.error('Erro ao processar o arquivo:', error);
+                return [];
+            });
+        return [];
     }
 }
