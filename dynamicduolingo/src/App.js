14a3dd1177c454f915892a282540bd0a6cfe7ab5
ccarlaa <carlacarlaclementino@gmail.com>
2024-09-06 21:47:30 -0300

    files refactor

---
+++ dynamicduolingo/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import DynamicDuolingo from './services/Translate'
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
